babf755177455859fa47b086c2d60ddd180b9c24
pehjiawang <138868756+pehjiawang@users.noreply.github.com>
2026-03-27 03:51:50 +0800

    Add files via upload

---
+++ llm_judge/homogeneity/llm_judge_evaluate_homogeneity_MemeTT.py
@@ -0,0 +1,37 @@
+### --------------------------------------------------------------------------------------------------------- ###
+### ---------------------------------------------Import libraries-------------------------------------------- ###
+### --------------------------------------------------------------------------------------------------------- ###
+import json
+import time
+
+from lightning_sdk import Machine, Studio
+from time import perf_counter
+
+### --------------------------------------------------------------------------------------------------------- ###
+### ------------------------------------------------Run studios---------------------------------------------- ###
+### --------------------------------------------------------------------------------------------------------- ###
+llm_judge_evaluate_homogeneity_MemeTT_start = perf_counter()
+llm_judge_evaluate_homogeneity_MemeTT_studio = Studio("6001_llm_judge_evaluate_homogeneity")
+llm_judge_evaluate_homogeneity_MemeTT_studio.start(Machine.DATA_PREP)
+time.sleep(100)
+llm_judge_evaluate_homogeneity_MemeTT_studio.run("python -m pip install --upgrade pip")
+llm_judge_evaluate_homogeneity_MemeTT_studio.run("pip install -r /teamspace/uploads/requirements_OpenAI.txt")
+llm_judge_evaluate_homogeneity_MemeTT_studio.run("python 6001_llm_judge_evaluate_homogeneity.py \
+    --inputfile /teamspace/studios/5000-hierarchical-clustering/clusters/memes_stage5000_completed.csv \
+    --clustercol narrative \
+    --outputfile /teamspace/studios/6001-llm-judge-evaluate-homogeneity/evaluations/memes_stage6001_completed.csv \
+    --inputprompts /teamspace/uploads/prompts_homogeneity_evaluation.json \
+    --inputcred /teamspace/uploads/OPENAI_CREDENTIALS.env")
+llm_judge_evaluate_homogeneity_MemeTT_studio.stop()
+llm_judge_evaluate_homogeneity_MemeTT_stop = perf_counter()
+llm_judge_evaluate_homogeneity_MemeTT_time = llm_judge_evaluate_homogeneity_MemeTT_stop - llm_judge_evaluate_homogeneity_MemeTT_start
+
+### --------------------------------------------------------------------------------------------------------- ###
+### ---------------------------------------------Record time taken------------------------------------------- ###
+### --------------------------------------------------------------------------------------------------------- ###
+time_taken_dict = {
+    "llm_judge_evaluate_homogeneity_MemeTT_time": str(llm_judge_evaluate_homogeneity_MemeTT_time),
+}
+time_taken_json = json.dumps(time_taken_dict)
+with open(file="llm_judge_evaluate_homogeneity_MemeTT_time_taken.json", mode="w", encoding="utf-8") as outfile:
+    outfile.write(time_taken_json)
